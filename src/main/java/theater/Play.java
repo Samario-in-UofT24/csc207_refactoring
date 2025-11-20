295d1f0482be80df7168281f507b044985aecfa2
samario <samario.liu@mail.utoronto.ca>
2025-11-19 22:53:48 -0500

    Finish Task 1.1

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,24 @@
 package theater;
 
+/**
+ * This is a short description for Plays.
+ * We need correct grammar and punctuation here.
+ */
 public class Play {
 
-    public String name;
-    public String type;
+    private final String name;
+    private final String type;
 
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
     }
 }
